4f17feb0e1ce57eb332275cbdc3e03465215e371
hikaruikebe <hikaru.ikebe@columbia.edu>
2024-03-23 20:45:34 -0400

    add scrolling

---
+++ data/sqlite.sql
@@ -0,0 +1,5 @@
+-- SQLite
+select * from tabelogs where prefecture_english = "Aomori" or prefecture_english = "Miyagi" ORDER BY score;
+
+
+-- select tabelog_lunch_budget_min, tabelog_lunch_budget_max from tabelogs where store_name = "米山";
